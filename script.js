4be11e07ece92b551943f9ee8032992a848894f2
Levis <anton_levus@mail.ru>
2023-01-18 23:16:54 +0530

    refactoring

--- script.js
+++ script.js
@@ -14,56 +14,65 @@
 об этом пользователю.
 */
 
-const getTernaryNumArray = (integer) => {
-    const value = 3;
-    let result = '';
-    while (integer > 0) {
-      result += integer % value;
-      integer = Math.floor(integer / value);
-    }
-    return result.split('');
-  };
-  
-  const getMathExpression = (num) => {
-    let ternaryArray = getTernaryNumArray(num);
-    let emptyArray = new Array(9).fill('');
-  
-    for (let i = 1; i <= 10; i += 1) {
-      if (i <= ternaryArray.length) {
-        if (Number(ternaryArray[i]) === 1) emptyArray[i] = '+';
-        if (Number(ternaryArray[i]) === 2) emptyArray[i] = '-';
-      } else {
-        emptyArray[i] = '';
-      };
-    };
-  
-    let mathExpression = '';
-    for (let i = 0; i < 9; i++) {
-      mathExpression += (9 - i) + emptyArray[i];
-    }
-    mathExpression += '0';
-    return mathExpression;
+const mapNumberToSign = {
+  0: '',
+  1: '+',
+  2: '-'
+};
+
+const convertDecimalToTernary = (num) => {
+  const value = 3;
+  let result = '';
+  while (num > 0) {
+    result += num % value;
+    num = Math.floor(num / value);
   }
-  
-  const  getSolution = (number) => {
-    const results = {};
-    let result = [];
-    const quantity = Math.pow(3, 9);
-  
-    for (let i = 0; i < quantity; i++) {
-      const expression = getMathExpression(i);
-      const result = eval(expression);
-      
-      if (result in results) {
-        results[result].push(expression);
-      } else {
-        results[result] = [expression];
-      }
-    }
-  
-    if (number in results) {
-      return results[number]
-    }
-    return 'Решений нет';
+
+  return result;
+}
+
+const getTernaryNumArray = (num) => {
+  const ternaryString = convertDecimalToTernary(num);
+  return ternaryString.split('').map(num => Number(num));
+};
+
+const getMathExpression = (num) => {
+  const ternaryArray = getTernaryNumArray(num);
+  const emptyArray = new Array(9).fill('');
+
+  for (let i = 1; i < ternaryArray.length; i += 1) {
+    emptyArray[i] = mapNumberToSign[ternaryArray[i]]
   };
-  
+
+  let mathExpression = '';
+  for (let i = 0; i < 9; i++) {
+    mathExpression += (9 - i) + emptyArray[i];
+  }
+  mathExpression += '0';
+
+  return mathExpression;
+}
+
+const getNumberSolutions = (number) => {
+  const mapNumToSolutions = {};
+  const quantity = 3 ** 9;
+
+  for (let i = 0; i < quantity; i++) {
+    const expression = getMathExpression(i);
+    const calculatedExpression = eval(expression);
+
+    if (calculatedExpression in mapNumToSolutions) {
+      mapNumToSolutions[calculatedExpression].push(expression);
+    } else {
+      mapNumToSolutions[calculatedExpression] = [expression];
+    }
+  }
+
+  if (number in mapNumToSolutions) {
+    return [...new Set(mapNumToSolutions[number])]
+  }
+
+  return 'Решений нет';
+};
+
+console.log(getNumberSolutions(200))
